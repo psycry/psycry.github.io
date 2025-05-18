084bcca280995eace88a6f855073f56a5bd2b49a
PsyCry (Jasper) <psycry@proton.me>
2025-05-18 03:18:14 -0400

    Adding logos for nomad nexus

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,12 @@ function App() {
       {/*  </a>*/}
       {/*</div>*/}
       <h1>Digital Nomad LLC</h1>
-        <a href = "https://play.google.com/store/apps/details?id=com.psycry.nomad_nexus">Nomad Nexus on Google Play</a>
+        <a href = "https://play.google.com/store/apps/details?id=com.psycry.nomad_nexus">
+            <p>
+                <img src="/nomad_nexus.png" className="logo nomad_nexus" alt="Nomad Nexus logo" /><br />
+                Nomad Nexus on Google Play
+            </p>
+        </a>
       {/*<div className="card">*/}
       {/*  <button onClick={() => setCount((count) => count + 1)}>*/}
       {/*    count is {count}*/}
