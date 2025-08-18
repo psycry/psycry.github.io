16aa4429c198724740926679c75a62b7514f5609
PsyCry (Jasper) <psycry@proton.me>
2025-08-17 21:33:54 -0400

    Changing border to not change colour on hover

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -24,7 +24,7 @@ const ProductsPage: React.FC = () => (
             <div className="container mx-auto px-6 text-center">
                 <h2 className="text-3xl font-bold text-slate-100 mb-12">Products</h2>
                 <div className="grid md:grid-cols-2 gap-8 max-w-5xl mx-auto">
-                    <div className="bg-slate-900 p-8 rounded-lg border border-slate-700">
+                    <div className="bg-slate-900 p-8 rounded-lg border border-purple-500" style={{transform: 'none !important', transition: 'none !important'}}>
                         <img src={"shelter_source_small.png"} alt="Shelter Source App" />
                         <h3 className="text-2xl font-bold text-white mb-4">Shelter Source</h3>
                         {/*<p className="text-slate-400 mb-6">From personal portfolios to automated business platforms, we build the custom technology that sets you free.</p>*/}
